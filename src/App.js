ffd4978bec9a333a131ee0090087380992be9661
AJ Diaz <ajdiaz@trouthousetech.com>
2023-04-11 15:38:49 -0600

    create a simple form with some basic styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import './styles/Main.css'
+import './styles/Form.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="main">
+      <header className="main-header" >
+      <div className = "form-box">
+        <h5 className = "form-step"> Ski Check-in </h5>
+        <form>
+            <div className = "field1">
+            <label> customer info </label>
+            <input placeholder="Name"/>        
+            <input placeholder="Phone 000-000-0000"/>
+            <input placeholder="E-mail"/>
+            <textarea placeholder="Ski info"/>
+            <textarea placeholder="Boot info"/>
+            <textarea placeholder="Binding info"/>
+            </div>
+
+            <button type = "submit" id= "submitBtn" className = "submitBtn"> submit</button>
+        </form>
+
+    </div>
       </header>
+      
     </div>
   );
 }
